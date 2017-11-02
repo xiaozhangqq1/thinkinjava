b95c0f45ac970757ad8838d5b72ac78ca61c0110
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-02 23:19:36 +0800

    add some test

---
+++ src/chapter7/Homer.java
@@ -0,0 +1,13 @@
+package chapter7;
+
+public class Homer {
+    char doh(char c){
+        System.out.println("doh(char)");
+        return 'd';
+    }
+    float doh(float f){
+        System.out.println("doh(float)");
+        return 1.0f;
+    }
+
+}
