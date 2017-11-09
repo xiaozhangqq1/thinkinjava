a8d09a579d99189c3ea1f8743197af4d677fc1d9
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-09 23:09:11 +0800

    add some test

--- src/chapter7/FinalOverridingIllusion.java
+++ src/chapter7/FinalOverridingIllusion.java
@@ -2,3 +2,24 @@ package chapter7;
 
 public class FinalOverridingIllusion {
 }
+class WithFinal{
+    private final void f(){
+        System.out.println("WithFinals.f()");
+    }
+    private void g(){
+        System.out.println("WithFinals.g()");
+    }
+    class OverridingPravite extends WithFinal{
+        private final void f(){
+            System.out.println("OverridingPrivate.f()");
+        }
+        private void g(){
+            System.out.println("OverridingPrivate.g()");
+        }
+    }
+    class OverridingPravite2 extends OverridingPravite{
+        public final void f(){
+            System.out.println("OverridingPrivate2.f() ");
+        }
+    }
+}
