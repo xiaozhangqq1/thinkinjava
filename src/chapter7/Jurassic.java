0d67d65b8d598f28236ce650e45a5853e3201a94
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-14 22:42:50 +0800

    add some test

---
+++ src/chapter7/Jurassic.java
@@ -0,0 +1,19 @@
+package chapter7;
+
+public class Jurassic {
+    public static void main(String[] args){
+        Dinosaur n = new Dinosaur();
+        n.f();
+        n.i = 40;
+        n.j++;
+    }
+}
+
+final class Dinosaur{
+    int i = 7;
+    int j = 1;
+    SmallBrain x = new SmallBrain();
+    void f(){}
+}
+
+class SmallBrain{}
