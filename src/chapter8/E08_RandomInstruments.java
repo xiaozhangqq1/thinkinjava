c5f0c653ae66a9008d3431a954246fa4ca033d46
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-23 22:53:03 +0800

    add some test

---
+++ src/chapter8/E08_RandomInstruments.java
@@ -0,0 +1,21 @@
+package chapter8;
+
+import java.util.Random;
+
+public class E08_RandomInstruments {
+    public static void main(String[] args) {
+        InstrumentGenerator gen = new InstrumentGenerator();
+        for (int i = 0;i<20;i++){
+            System.out.println(gen.next());
+        }
+    }
+}
+class InstrumentGenerator{
+    Random gen = new Random(47);
+    public Instrument next(){
+        switch (gen.nextInt(6)){
+            default:
+            case 0: return new Wind();
+        }
+    }
+}
