a51297f1fe3ecc05c2002e1fc4496b5a27345c46
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-12-04 16:49:20 +0800

    add some practise

--- src/chapter8/E09_Rodents.java
+++ src/chapter8/E09_Rodents.java
@@ -4,26 +4,48 @@ package chapter8;
  * Created by zhangxuelong on 2017/11/27
  */
 public class E09_Rodents {
+    public static void main(String[] args) {
+        Rodent rodent = new Mouse();
+        rodent.hop();
+        rodent.reproduce();
+        rodent.reproduce();
+        System.out.println(rodent);
+    }
 
 }
 
-class Rodent{
-    public void hop(){
+class Rodent {
+    public void hop() {
         System.out.println("Rodent hopping");
     }
-    public void scurry(){
+
+    public void scurry() {
         System.out.println("Rodent scurrying");
     }
-    public void reproduce(){
+
+    public void reproduce() {
         System.out.println("Making more Rodents");
     }
-    public String toString(){
+
+    public String toString() {
         return "Rodent";
     }
 }
 
-class Mouse extends Rodent{
-    public void hop(){
+class Mouse extends Rodent {
+    public void hop() {
         System.out.println("Mouse hopping");
+    }
+
+    public void scurry() {
+        System.out.println("mouse scurrying");
+    }
+
+    public void reproduce() {
+        System.out.println("Making more Mice");
+    }
+
+    public String toString() {
+        return "Mouse";
     }
 }
