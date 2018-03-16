3450ba2c19e6c63a1daca36fc232f4d8a89be05a
zhangxuelong10 <xuelongzhang@vivedu.com>
2018-03-16 09:45:23 +0800

    Chapter1 ,1.1.11

---
+++ src/Algorithns/Practice1111.java
@@ -0,0 +1,16 @@
+package Algorithns;
+
+/**
+ * Created by zhangxuelong on 2018/3/16
+ */
+public class Practice1111 {
+
+    public static void main(String[] args) {
+        String[][] arr = {{"*", ""},{"","*"}};
+        for (int i = 0;i<arr.length;i++){
+            for (int j = 0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
+            }
+        }
+    }
+}
