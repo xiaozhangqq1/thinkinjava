22c5c580c32e820df2bdc6a585067a2e088adbca
zhangxuelong10 <xuelongzhang@vivedu.com>
2018-03-19 13:45:30 +0800

    1113

--- src/Algorithns/Practice1113.java
+++ src/Algorithns/Practice1113.java
@@ -5,7 +5,13 @@ package Algorithns;
  */
 public class Practice1113 {
     public static void main(String[] args) {
-        int[][] arr = new int[3][5];
-
+        int[][] arr = new int[2][3];
+        int[][] a = {{1, 2, 3}, {4, 5, 6}};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + "");
+            }
+            System.out.println();
+        }
     }
 }
